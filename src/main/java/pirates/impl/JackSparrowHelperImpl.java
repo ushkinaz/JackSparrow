dd246fce4fdb33d0f97e380f83a464083bbe8f5a
Dmitry Sidorenko <rustydragon@gmail.com>
2015-12-11 21:49:09 +0300

    Dummy impl

---
+++ src/main/java/pirates/impl/JackSparrowHelperImpl.java
@@ -0,0 +1,12 @@
+package pirates.impl;
+
+import pirates.JackSparrowHelper;
+import pirates.Purchases;
+
+public class JackSparrowHelperImpl implements JackSparrowHelper {
+    @Override
+    public Purchases helpJackSparrow(String path, int numberOfGallons) {
+        //TODO: Implement pirates.impl.JackSparrowHelperImpl#helpJackSparrow
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
