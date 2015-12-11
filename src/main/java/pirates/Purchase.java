b29a3c99f3ba9eac380be5306103eed6990307f1
Dmitry Sidorenko <rustydragon@gmail.com>
2015-12-11 21:44:03 +0300

    Java with style

--- src/main/java/pirates/Purchase.java
+++ src/main/java/pirates/Purchase.java
@@ -1,7 +1,16 @@
 package pirates;
 
 public class Purchase {
-    String sourceName; // название источника в файле sources.csv
-    Integer numberOfGallons; // число галлонов, купленное у этого источника
-    Double priceOfGallon; // цена галлона у этого источника
+    /**
+     * название источника в файле sources.csv
+     */
+    String sourceName;
+    /**
+     * число галлонов, купленное у этого источника
+     */
+    Integer numberOfGallons;
+    /**
+     * цена галлона у этого источника
+     */
+    Double priceOfGallon;
 }
--- src/main/java/pirates/Purchases.java
+++ src/main/java/pirates/Purchases.java
@@ -3,7 +3,16 @@ package pirates;
 import java.util.TreeSet;
 
 public class Purchases {
-    Integer numberOfGallons; // совокупное число галлонов (из условия)
-    TreeSet<Purchase> purchases; // набор покупок в разных источниках
-    Double averagePriceOfGallon; // средняя цена галлона по всем источникам
+    /**
+     * совокупное число галлонов (из условия)
+     */
+    Integer numberOfGallons;
+    /**
+     * набор покупок в разных источниках
+     */
+    TreeSet<Purchase> purchases;
+    /**
+     * средняя цена галлона по всем источникам
+     */
+    Double averagePriceOfGallon;
 }
