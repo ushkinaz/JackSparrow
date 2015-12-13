21271561f356c368ce4c313672b5ad2428a3259a
Dmitry Sidorenko <rustydragon@gmail.com>
2015-12-13 23:22:42 +0300

    jdoc fix

--- src/main/java/pirates/JackSparrowHelper.java
+++ src/main/java/pirates/JackSparrowHelper.java
@@ -2,9 +2,9 @@ package pirates;
 
 public interface JackSparrowHelper {
     /**
-     * param path            Полный путь к csv файлу с ценами и количествами (sources.csv), доступными в разных местах
-     * param numberOfGallons совокупное количество галлонов, нужное Джеку
-     * return <tt>Purchases</tt> детальный совет Джеку, где и в каком количестве покупать ром
+     * @param path            Полный путь к csv файлу с ценами и количествами (sources.csv), доступными в разных местах
+     * @param numberOfGallons совокупное количество галлонов, нужное Джеку
+     * @return <tt>Purchases</tt> детальный совет Джеку, где и в каком количестве покупать ром
      */
     Purchases helpJackSparrow(String path, int numberOfGallons);
 }
